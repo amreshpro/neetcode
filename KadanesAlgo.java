d7f2b8f54aa67e19edff1be631f2376339765540
Amresh Maurya <amresh.terminal@gmail.com>
2025-02-19 19:48:05 +0530

    Update KadanesAlgo.java

--- KadanesAlgo.java
+++ KadanesAlgo.java
@@ -1,7 +1,7 @@
 // subarray max sum
 public class KadanesAlgo {
     public static int kadanesAlgoSum(int[] arr){
-        int maxSum= arr[0];
+        int maxSum = arr[0];
         int currentSum = arr[0];
         for(int i = 1; i < arr.length; i++){
             currentSum = Math.max(arr[i],currentSum + arr[i]);
@@ -21,7 +21,7 @@ public class KadanesAlgo {
              int sum = 0;
             for(int j = i; j < arr.length; j++){
                 sum += arr[j];
-                maxSum= Math.max(sum,maxSum);
+                maxSum = Math.max(sum,maxSum);
             }   
         }
         return maxSum;
@@ -34,7 +34,5 @@ public class KadanesAlgo {
         int res2 = kadanesAlgoSum(arr);
         System.out.println("Result->BruteForce: " + res);
         System.out.println("Result->KadanesAlgo: " + res2);
-        
-        
     }
 }
